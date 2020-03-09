4396de6a69acd40f4108df5abc955cdde0faaceb
haxzie <musthu.gm@gmail.com>
2020-03-09 10:13:06 +0530

    Fix browser caching

--- src/api/covid19.js
+++ src/api/covid19.js
@@ -1,15 +1,17 @@
 import { covid19Api } from './client';
 
+// Send across the timestamp so that the browser doesn't cache the responses
+
 const fetchLayers = () => {
-    return covid19Api.get('/layers.json');
+    return covid19Api.get(`/layers.json?ts=${new Date().getTime()}`);
 }
 
 const fetchScatterplotLayer = () => {
-    return covid19Api.get('/sctterplot.json');
+    return covid19Api.get(`/sctterplot.json?ts=${new Date().getTime()}`);
 }
 
 const fetchGeoJSONLayer = () => {
-    return covid19Api.get('/geo.json');
+    return covid19Api.get(`/geo.json?ts=${new Date().getTime()}`);
 }
 
 export {
